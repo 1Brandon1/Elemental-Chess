7805abfb7fa00fac2de4951c544b578553ed4245
Brandon <Brandon@Attram.net>
2025-11-19 02:11:32 +0000

    Improved game keybind code

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,17 @@ const game = new Game()
 
 game.start('pvp', 'elemental')
 
-document.addEventListener('keydown', (event) => {
-	switch (event.key) {
-		case 'ArrowLeft':
-			game.undoMove()
-			break
-		case 'ArrowRight':
-			game.redoMove()
-			break
-		case 'f':
-			game.board.flip()
-			break
-		case 'n':
-			newGame(game.gameType)
-			break
-	}
+//!-------------- Keybind actions --------------
+const actions = {
+	ArrowLeft: () => game.undoMove(), // Undo move
+	ArrowRight: () => game.redoMove(), // Redo move
+	f: () => game.board.flip(), // Flip board
+	n: () => newGame(game.gameType), // New game
+	r: () => openRules(), // Show rules
+	t: () => toggleTheme() // Change theme
+}
+
+// Key listener
+document.addEventListener('keydown', ({ key }) => {
+	if (actions[key]) actions[key]()
 })
